36d06231a387444d510698e4118e552330633ce0
IngabireTina <ingabire067@gmail.com>
2021-03-03 02:47:45 -0500

    added find-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FindFormComponent } from './find-form/find-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FindFormComponent
   ],
   imports: [
     BrowserModule,

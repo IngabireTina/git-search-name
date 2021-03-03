b584d6b605c06cefbf80985cc50a9bd5ce64fd9f
IngabireTina <ingabire067@gmail.com>
2021-03-03 02:49:09 -0500

    added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FindFormComponent } from './find-form/find-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FindFormComponent
+    FindFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
